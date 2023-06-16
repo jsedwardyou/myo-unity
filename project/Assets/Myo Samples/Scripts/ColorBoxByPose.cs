d7041a1dde8788153d4d49ccc05e91c4e814ee7d
kwkim <Stevekim543@gmail.com>
2023-06-16 11:14:57 +0900

    install myo unity package

--- project/Assets/Myo Samples/Scripts/ColorBoxByPose.cs
+++ project/Assets/Myo Samples/Scripts/ColorBoxByPose.cs
@@ -1,17 +1,3 @@
-// Copyright 2015 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 ﻿using UnityEngine;
 using System.Collections;
 
@@ -60,15 +46,15 @@ public class ColorBoxByPose : MonoBehaviour
 
             // Change material when wave in, wave out or double tap poses are made.
             } else if (thalmicMyo.pose == Pose.WaveIn) {
-                renderer.material = waveInMaterial;
+                GetComponent<Renderer>().material = waveInMaterial;
 
                 ExtendUnlockAndNotifyUserAction (thalmicMyo);
             } else if (thalmicMyo.pose == Pose.WaveOut) {
-                renderer.material = waveOutMaterial;
+                GetComponent<Renderer>().material = waveOutMaterial;
 
                 ExtendUnlockAndNotifyUserAction (thalmicMyo);
             } else if (thalmicMyo.pose == Pose.DoubleTap) {
-                renderer.material = doubleTapMaterial;
+                GetComponent<Renderer>().material = doubleTapMaterial;
 
                 ExtendUnlockAndNotifyUserAction (thalmicMyo);
             }
